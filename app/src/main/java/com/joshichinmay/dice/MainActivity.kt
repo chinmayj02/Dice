f01692cca852443e0fbcef842572a776599f235b
Chinmay Joshi <chinmayujoshi@gmail.com>
2022-01-31 10:26:32 +0530

    basic dice roll

--- app/src/main/java/com/joshichinmay/dice/MainActivity.kt
+++ app/src/main/java/com/joshichinmay/dice/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.joshichinmay.dice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice()
+        val diceRoll = dice.roll()
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice {
+    var sides = 6
+    fun roll(): Int {
+        return (1..sides).random()
     }
 }
